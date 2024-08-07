3ae2931e44b7fed570c98bd2d0a131d7658c66a0
Jaydev162 <jaydevdevre162@gmail.com>
2024-08-07 07:37:17 -0700

    Add files via upload

---
+++ home.php
@@ -0,0 +1,85 @@
+<html>
+<head>
+<title>
+college website templet
+</title>
+<link rel="stylesheet" href="css/style.css">
+</head>
+<body background="b.jpg" >
+<center>
+<img src="mgm.jpg" class="logo"><BR>
+<FONT SIZE=30PX color="white">
+MGM COLLEGE OF CS & IT , NANDED</CENTER>
+</FONT><br><HR COLOR="green">
+<marquee bgcolor="white" >
+WELCOME.... 
+</marquee>
+<HR COLOR="blue"><br>
+<div class="container1">
+<nav>
+<a href="home.php">home</a>
+<a href="register.php">register</a>
+<a href="login.php">login</a>
+<a href="about.php">about</a>
+<a href="help.php">help</a>
+<a href="contact.php">contact us</a>
+</nav>
+
+</div><br><br><Br>
+<DIV CLASS="main-section">
+<img src="m.png"><br><BR>
+</div>
+<div class="about">
+<DIV CLASS="main-section">
+<div class="container">
+<div class="event">
+<DIV CLASS="heading">
+NOTICE BOARD</DIV>
+<DIV>
+<marquee bgcolor="blue"><font color="white">new</font></marquee>
+<ul>
+ <li>today is holiday
+ <li>today is holiday
+ <li>today is holiday
+</ul>
+</DIV>
+</div>
+<div class="event">
+<DIV CLASS="heading">
+IMPORTANT DATES
+</DIV>
+<DIV>
+<marquee bgcolor="blue"><font color="white">new</font></marquee>
+<ul>
+ <li>today is holiday
+ <li>today is holiday
+ <li>today is holiday
+</ul>
+</DIV>
+</div>
+<div class="event">
+<DIV CLASS="heading">
+IMPORTANT 
+</DIV>
+<DIV>
+<marquee bgcolor="blue"><font color="white">new</font></marquee>
+<ul>
+ <li>today is holiday
+ <li>today is holiday
+ <li>today is holiday
+</ul>
+</DIV></div></DIV></div></div>
+<div class="main-section">
+<font color="white" size=30px><br><hr color="blue"><hr color="blue">
+ANY SEARCH
+<form action="save.php" method="post" >     
+  <input type="text" placeholder="Search.."><br><br>
+  <input type="submit"><br>
+  </form><br><hr color="blue"><hr color="blue">
+    <marquee bgcolor="green">
+  contact us :
+  <br>
+  <img src="phone1.png" width=30px >+91-3526417895
+  </marquee></font>
+</body>
+</html>
